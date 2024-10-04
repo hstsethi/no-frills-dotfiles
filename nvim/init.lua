6b34c9e5ce767ff5df2b0ffe2d6277bbed8ddef3
HstSethi <hstsethi@outlook.com>
2024-10-04 06:34:43 -0400

    Modularize NVim Config

--- nvim/init.lua
+++ nvim/init.lua
@@ -22,51 +22,6 @@ vim.keymap.set("n", "<leader>dt", function()
 end, { noremap = true }) 
 vim.keymap.set("n", "<leader>f", "<cmd>FzfLua<CR>")
 vim.keymap.set("n", "<leader>r", ":make<CR>:copen", { noremap = true, silent = true })
-
-vim.api.nvim_create_autocmd("FileType", {
-    -- This method is way way faster, easier and vim-like than LSP. Compiler file "python" isn't shipped by default, hence needs to be installed first.
-    -- The % option can be added on either make command or in here, but I have added it here so make command can be used with other enviorments too.
-	pattern = "python",
-	callback = function()
-		vim.cmd([[compiler python]])
-		vim.cmd([[set makeprg=python3\ \%]])
-		vim.bo.omnifunc = "v:lua.vim.lsp.omnifunc"
-	end,
-})
-
--- LSP Related shortcuts
-vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.declaration()<CR>", { silent = true })
-vim.api.nvim_set_keymap("n", "<c-]>", "<cmd>lua vim.lsp.buf.definition()<CR>", { silent = true })
-vim.api.nvim_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { silent = true })
-vim.api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.implementation()<CR>", { silent = true })
-vim.api.nvim_set_keymap("n", "<c-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", { silent = true })
-vim.api.nvim_set_keymap("n", "1gD", "<cmd>lua vim.lsp.buf.type_definition()<CR>", { silent = true })
-vim.api.nvim_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", { silent = true })
-vim.api.nvim_set_keymap("n", "g0", "<cmd>lua vim.lsp.buf.document_symbol()<CR>", { silent = true })
-vim.api.nvim_set_keymap("n", "gW", "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>", { silent = true })
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
-require("lazy").setup({
-	spec = {
-		"neovim/nvim-lspconfig",
-		"folke/which-key.nvim",
-		"numtoStr/comment.nvim",
-		"ibhagwan/fzf-lua", -- Telebloat not required
-	},
-})
-require("Comment").setup()
-require("which-key").setup()
--- require("lspconfig").gopls.setup({})
--- require("lspconfig").tsserver.setup({})
-require("lspconfig").pyright.setup({})
+require("plguins")
+require("python")
+require("lsp")
